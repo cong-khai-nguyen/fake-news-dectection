d159931093d0c3a509d2ae02cc6c9cfb7fd76d75
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-12 11:48:21 -0500

    import data

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+df = pd.read_csv('data/train.csv')
+
+# print(df.head())
+
+x = df.drop('label', axis = 1)
+# print(x.head())
+y = df['label']
+# print(y.head())
+
+print(df.shape)
