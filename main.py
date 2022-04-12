80c7b7fc4b43c33ca10f8b127f459848fe187e15
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-12 12:24:43 -0500

    pre-processing data

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import re as regex
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
+ps = PorterStemmer()
 df = pd.read_csv('data/train.csv')
 
 # print(df.head())
@@ -21,3 +24,15 @@ print(df.shape)
 
 copy = df.copy()
 copy.reset_index(inplace=True)
+
+print(copy['title'][2])
+
+# Replace all the special characters in the title column with space to create a bag of words
+corpus = []
+
+for i in range(len(copy)):
+    review = regex.sub('[^a-z-A-Z]', ' ', copy['title'][i])
+    review = review.lower()
+    review = review.split()
+    # stem all the words in the title if those words are not the stopwords list
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
