446fd8929da34d36ecb1361f9e814bfc3e894fb0
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-14 11:43:20 -0500

    confusion matrix plot function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
 import re as regex
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 import pickle
+import itertools
+import matplotlib.pyplot as plt
 # import nltk
 # nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -64,6 +67,42 @@ except:
     classifer.fit(x_train, y_train)
     with open("model.pickle", "wb") as f:
         pickle.dump((classifer), f)
+
+# Credit: https://www.datacamp.com/community/tutorials/scikit-learn-fake-news
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    See full source and example:
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
 
 pred = classifer.predict(x_test)
 score = metrics.accuracy_score(pred, y_test)
